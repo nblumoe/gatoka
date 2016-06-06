5a0fd513ae84a5baeac97d742f80a023d0c4e0e5
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-06-06 20:24:03 +0200

    Add basic HTTP response

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ respond = do
+  putStrLn "I've done some IO here"
+  respond $ responseLBS
+    status200
+    [("Content-Type", "text/plain")]
+    "Hello Web!"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "http://localhost:8080/"
+  run 8080 app
