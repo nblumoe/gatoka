966b3088f2b9798b1a570fe2a0a8467b0bee697d
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-06-07 17:20:57 +0200

    Add basic routing

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
 import Network.HTTP.Types
+import Network.HTTP.Types.Header
 import Network.Wai.Handler.Warp (run)
 
-app :: Application
-app _ respond = do
-  putStrLn "I've done some IO here"
-  respond $ responseLBS
-    status200
-    [("Content-Type", "text/plain")]
-    "Hello Web!"
+app :: Request -> (Response -> t) -> t
+app request respond
+  | isTracking = respond tracking
+  | otherwise  = respond notFound
+  where isTracking = elem pathRoot ["w", "k"]
+        pathRoot = (head $ pathInfo request)
 
 main :: IO ()
 main = do
-  putStrLn "http://localhost:8080/"
+  putStrLn "Server started on: http://localhost:8080/"
   run 8080 app
+
+defaultHeader
+  :: [(HeaderName, Data.ByteString.Internal.ByteString)]
+defaultHeader = [(hContentType, "text/plain")]
+
+tracking :: Response
+tracking = responseLBS
+  status200
+  defaultHeader
+  "200 - Tracking call received"
+
+notFound :: Response
+notFound = responseLBS
+  status404
+  defaultHeader
+  "404 - Not Found"
