a1324d3a0ada102947388dde6b59b03c7ba974b1
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-06-12 12:47:52 +0200

    Run producer in a thread, send payload via channel

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Control.Concurrent
+import Control.Monad (forever)
+import Control.Concurrent.Chan
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import           Haskakafka
@@ -13,14 +16,21 @@ main = do
     kafkaConfig = [("socket.timeout.ms", "50000")]
     topicConfig = [("request.timeout.ms", "50000")]
   putStrLn "Server started on: http://localhost:8080/"
-  withKafkaProducer kafkaConfig topicConfig
+  producerChan <- newChan
+  _ <- forkIO $ withKafkaProducer kafkaConfig topicConfig
     "broker2:9092" "sandbox"
-    $ \_ topic -> run 8080 $ app topic
+    $ \_ topic ->  forever $ do
+    payload <- readChan producerChan
+    produceMessage topic KafkaUnassignedPartition
+      $ KafkaProduceMessage payload
+  run 8080 $ app jsonFromRequest producerChan
 
-app:: KafkaTopic -> Application
-app topic request respond
+app
+  :: (Request -> B.ByteString)
+     -> Chan B.ByteString -> Request -> (Response -> IO b) -> IO b
+app payloadFromRequest producerChan request respond
   | isTracking = do
-      _ <- produceMessage topic KafkaUnassignedPartition $ KafkaProduceMessage payload
+      writeChan producerChan payload
       respond $ messageProduced $ BL.fromStrict payload
   | otherwise  = respond notFound
   where isTracking = pathRoot `elem` ["w", "k"]
@@ -29,15 +39,16 @@ app topic request respond
 
 -- Payload handling
 
-payloadFromRequest :: Request -> B.ByteString
-payloadFromRequest request = B.concat ["{", query_items, "}"]
+jsonFromRequest :: Request -> B.ByteString
+jsonFromRequest request = B.concat ["{", query_items, "}"]
   where query_items = B.intercalate ", " $ map formatQueryItem $ queryString request
 
 formatQueryItem :: QueryItem -> B.ByteString
 formatQueryItem ("", _) = ""
-formatQueryItem (k, Just "") = B.append k ": undefined"
 formatQueryItem (k, Nothing) = B.append k ": undefined"
-formatQueryItem (k, Just v) = B.concat [k, ": ",  v]
+formatQueryItem (k, Just v)
+  | v == "" = B.append k ": undefined"
+  | otherwise = B.concat [k, ": ",  v]
 
 -- Response handling
 
